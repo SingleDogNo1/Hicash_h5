38839205afb5f0eb2319a84af15e817ba14dbf64
lizhen <281955666@qq.com>
2018-08-08 14:11:10 +0800

    ,

--- running.js
+++ running.js
@@ -13,7 +13,7 @@ fs.unlink(configPath, function(err){
     if(err){
         throw err;
     }
-    console.info(configPath, ' 删除成功！');
+    console.info(configPath, ' 旧的配置文件删除成功！');
 
     const _src = path.join(cwd, 'globalUrl', type, 'config.json');
     const _dist = path.join(cwd, 'src', 'config.json');
@@ -23,13 +23,13 @@ fs.unlink(configPath, function(err){
     var writeStream = fs.createWriteStream(_dist);
     
     readStream.on('data', function(chunk){
-        console.info(_src, ' 写入中 ...');
+        console.info(_src, ' 新配置文件写入中 ...');
         // 可读流收到data事件时，将内容写入到可写流
         writeStream.write(chunk);
     });
     
     readStream.on('end', function(){
-        console.info(_dist, ' 写入完成!');
+        console.info(_dist, ' 新的配置文件写入完成!');
     // 当可读流读取完成，会发出end事件，这时我们要把可写流关闭
         writeStream.end();
         // console.info(batPath, ' 执行中...');
@@ -43,11 +43,11 @@ fs.unlink(configPath, function(err){
 
         console.info('building ...');
 
-        child_process.exec('cd '+cwd +'& /mnt/jenkins/node/node-v10.5.0-linux-x64/bin/npm install --registry https://registry.npm.taobao.org  & /mnt/jenkins/node/node-v10.5.0-linux-x64/bin/npm run build',function (error, stdout, stderr) {
-            if (error !== null) {
-              console.log('exec error: ' + error);
-            }
-            console.info(stdout);
-        });
+        // child_process.exec('cd '+cwd +'& /mnt/jenkins/node/node-v10.5.0-linux-x64/bin/npm install --registry https://registry.npm.taobao.org  & /mnt/jenkins/node/node-v10.5.0-linux-x64/bin/npm run build',function (error, stdout, stderr) {
+        //     if (error !== null) {
+        //       console.log('exec error: ' + error);
+        //     }
+        //     console.info(stdout);
+        // });
     });
 });
