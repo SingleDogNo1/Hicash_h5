26e9ba52bdae242cf12a8d987d0d99a8ce0c90c4
lizhen <281955666@qq.com>
2018-09-21 15:06:23 +0800

    gps

--- src/app.vue
+++ src/app.vue
@@ -51,9 +51,11 @@ export default {
                 var gpsArr = [position.coords.longitude,position.coords.latitude];
                 _this.utils.setCookie("gpsArr", gpsArr);
                 console.info('Get GPS success', gpsArr);
+                alert('Get GPS success '+ JSON.stringify(gpsArr));
             },
             locationError=function (error) {
                 console.info('Get GPS failed', error);
+                alert('Get GPS failed '+ JSON.stringify(error));
             }
             getLocation();
         }
