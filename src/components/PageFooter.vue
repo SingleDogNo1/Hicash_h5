0225945fcbe31d648a13b2842d97764ef8fbdb1e
lizhen <281955666@qq.com>
2018-11-07 15:51:05 +0800

    remove other file\

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -8,7 +8,7 @@
             <span class="icon-broke-promise-area" slot="icon" style="position:relative;top: -2px;">&#xe637;</span>
             <span slot="label">失信专区</span>
         </tabbar-item>
-        <tabbar-item :link="{path: 'personal'}" :show-dot="personCenterDotShow" :selected="$route.path === '/personal'">
+        <tabbar-item :link="{path: 'personal'}" :show-dot="personCenterDotShow" :selected="$route.path === '/personal/personIndex'">
             <span class="icon-me" slot="icon" style="position:relative;top: -2px;">&#xe637;</span>
             <span slot="label">我的</span>
         </tabbar-item>
