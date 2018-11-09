92cdf15bd63a976f7f85549c552da76c145c0aeb
lizhen <281955666@qq.com>
2018-11-09 13:31:23 +0800

    我的分期

---
+++ src/pages/Personal/Instalment/Normal/normal.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        normal
+    </div>
+</template> 
+
+<style lang="scss" scoped>
+   
+</style>
+
+
+<script>
+
+    export default {
+        props: {
+
+        },
+        components: {
+            
+        },
+        data () {
+            return {
+            }
+        }
+    }
+</script>
---
+++ src/pages/Personal/Instalment/index.vue
@@ -0,0 +1,74 @@
+<template>
+	<div>
+		<page-header :title="title" :showBack="showBack" :showBtnClose="showBtnClose"></page-header>
+		<div class="content">
+			<tab custom-bar-width="60px" active-color="#FF7640" bar-active-color="#FF7640" v-model="index">
+				<tab-item selected @on-item-click="onItemClick" :key="0" >逾期订单</tab-item>
+				<tab-item @on-item-click="onItemClick" :key="1" >正常订单</tab-item>
+			</tab>
+			<swiper v-model="index" height="100px" :show-dots="false">
+				<swiper-item :key="0">
+					<instalment-normal></instalment-normal>
+				</swiper-item>
+				<swiper-item :key="1">
+					<instalment-overdue></instalment-overdue>
+				</swiper-item>
+			</swiper>
+		</div>
+		
+	</div>
+</template>
+<script type="text/javascript">
+	import {Tab, TabItem, Swiper, SwiperItem} from 'vux';
+	import PageHeader from '@/components/PageHeader.vue';
+	import InstalmentNormal from '@/pages/Personal/Instalment/Normal/normal.vue'
+	import InstalmentOverdue from '@/pages/Personal/Instalment/Overdue/overdue.vue'
+	export default {
+		components: {
+			Tab,
+			TabItem,
+			PageHeader,
+			Swiper,
+			SwiperItem,
+			InstalmentNormal,
+			InstalmentOverdue
+		},
+		data() {
+			return {
+				title: this.$router.history.current.meta.title,
+				showBtnClose: false,
+				showBack: true,
+				index: 0
+			}
+		},
+		mounted() {
+			
+		},
+		methods: {
+			onItemClick: function(type){
+				
+			}
+		}
+	}
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+	@import "~bowerComponents/sass-rem/_rem.scss";
+	.MyInstalment{
+		background: #f2f2f2;
+		.content{
+			height: calc(100% - rem(50px));
+			padding-top: rem(51px);
+			.vux-tab-wrap{
+				background: #fff;
+				border-bottom: 1px solid #f2f2f2;
+			}
+			.vux-tab-container{
+				width: 50%;
+			}
+			.vux-tab .vux-tab-item{
+				background: 0;
+			}
+		}
+	}
+</style>
