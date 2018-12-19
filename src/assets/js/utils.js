820b4a6961e60025d5ee9c098d17bbd95399f1a7
lizhen <281955666@qq.com>
2018-12-19 15:49:50 +0800

    .

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -44,7 +44,7 @@ export function getQueryString(name) {
 }
 
 export function checkMobile(mobile) {
-	var reg = /^1(3|4|5|7|8)\d{9}$/;
+	var reg = /^1(3|4|5|7|8|9)\d{9}$/;
 	return reg.test(mobile);
 }
 
