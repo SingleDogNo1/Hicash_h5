bbab9a8ba5180cd4498fe6e86c2ff7537fe6c586
lizhen <281955666@qq.com>
2019-01-14 10:22:09 +0800

    auth

---
+++ src/pages/Product/Auth/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		身份认证
+	</div>
+</template>
+
+<style lang="scss" rel="stylesheet/scss">
+@import "~bowerComponents/sass-rem/_rem.scss";
+
+</style>
+
+<script>
+// import {} from "vux";
+import axios from "axios";
+
+export default {
+	components: {
+		
+	}, 
+	data() {
+		return {
+			
+		};
+	},
+	methods: {
+		
+	},
+	mounted() {
+		
+	}
+};
+</script>
