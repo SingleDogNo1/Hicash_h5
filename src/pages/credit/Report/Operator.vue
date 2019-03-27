d1daa82c242d48acdd8398910a71a01da95c9293
wangzhe <wangzhe02@dpandora.cn>
2019-03-27 12:06:56 +0800

    .

--- src/pages/credit/Report/Operator.vue
+++ src/pages/credit/Report/Operator.vue
@@ -261,7 +261,7 @@ export default {
               desc: "征信报告分享",
               title: "征信报告分享",
               link: this.config.NEW_MWEB_PATH + "/activityIntroduction",
-              imgUrl: this.wxShareIco,
+              imgUrl: this.config.MWEB_PATH + this.wxShareIco,
               success: function() {},
               cancel: function() {}
             });
@@ -269,7 +269,7 @@ export default {
               desc: "征信报告分享",
               title: "征信报告分享",
               link: this.config.NEW_MWEB_PATH + "/activityIntroduction",
-              imgUrl: this.wxShareIco,
+              imgUrl: this.config.MWEB_PATH + this.wxShareIco,
               success: function() {},
               cancel: function() {}
             });
