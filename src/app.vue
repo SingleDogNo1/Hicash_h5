34ce734b1c97157ede85e856e91616d9e09e081e
zhangzheng <zhangzheng@dpandora.cn>
2019-03-27 17:47:57 +0800

    .

--- src/app.vue
+++ src/app.vue
@@ -1,29 +1,33 @@
 <template>
-	<div id="app" :class="[path, platform]">
-		<router-view></router-view>
-		<x-dialog v-model="authShowDialog" class="app-auth-dialog">
-			<div class="img-box">
-				<img src="./assets/images/pop-coupon.png" width="100%" alt="">
-        <p>恭喜获得 <span>{{couponAmount}}元</span> 优惠券</p>
-				<div class="btns">
-					<button @click="seeCoupon" class="auth-btn confirm">看优惠券</button>
-					<button @click="seeCredit" class="auth-btn cancel">看新报告</button>
-				</div>
-			</div>
-			<div @click="authShowDialog=false">
-				<span class="vux-close"></span>
-			</div>
-		</x-dialog>
+  <div id="app" :class="[path, platform]">
+    <router-view></router-view>
+    <x-dialog v-model="authShowDialog" class="app-auth-dialog">
+      <div class="img-box">
+        <img src="./assets/images/pop-coupon.png" width="100%" alt>
+        <p>
+          恭喜获得
+          <span>{{couponAmount}}元</span> 优惠券
+        </p>
+        <div class="btns">
+          <button @click="seeCoupon" class="auth-btn confirm">看优惠券</button>
+          <button @click="seeCredit" class="auth-btn cancel">看新报告</button>
+        </div>
+      </div>
+      <div @click="authShowDialog=false">
+        <span class="vux-close"></span>
+      </div>
+    </x-dialog>
     <confirm
       v-model="getReportSuccess"
       :close-on-confirm="false"
       title="获取报告成功"
       @on-confirm="onConfirmReport"
       confirm-text="看报告"
-      class="reportConfirm">
+      class="reportConfirm"
+    >
       <p style="text-align:center;">新鲜的报告已经准备好了，快来看看</p>
     </confirm>
-	</div>
+  </div>
 </template>
 
 <script>
@@ -32,15 +36,13 @@ import MiaoDai from "@/components/MiaoDai.vue";
 import { XDialog, Confirm } from "vux";
 
 export default {
-	name: "App",
-	created() {
-		//自动登录
-		let userName =
-			localStorage.getItem("userName") ||
-			this.utils.getCookie("userName");
-		let token =
-			localStorage.getItem("token") || this.utils.getCookie("token");
-		let _this = this;
+  name: "App",
+  created() {
+    //自动登录
+    let userName =
+      localStorage.getItem("userName") || this.utils.getCookie("userName");
+    let token = localStorage.getItem("token") || this.utils.getCookie("token");
+    let _this = this;
 
     if (userName && token) {
       this.jsCommon.setAuthorization(userName, token);
@@ -50,11 +52,18 @@ export default {
       document.title = this.$router.history.current.meta.title;
     }
 
-    console.info('this.$router.history.current', this.$router.history.current);
-    if(this.$router.history.current.query.source == 'iframe'){
-      parent.location.href = this.config.NEW_MWEB_PATH + this.$router.history.current.path;
+    console.info("this.$router.history.current", this.$router.history.current);
+    if (this.$router.history.current.query.source == "iframe") {
+      parent.location.href =
+        this.config.NEW_MWEB_PATH + this.$router.history.current.path;
     }
 
+    if (this.$route.query.source == "auth_iframe") {
+      var url = window.location.href;
+      var urlArr = url.split("&");
+      parent.location.href = urlArr[0];
+	}
+	
     if (this.$router.history.current.meta.requireAuth) {
       // 判断该路由是否需要登录权限
       if (!userName || userName == "null") {
@@ -100,141 +109,138 @@ export default {
       getLocation();
     }
   },
-	components: {
-		Home,
-		MiaoDai,
+  components: {
+    Home,
+    MiaoDai,
     XDialog,
     Confirm
-	},
-	mounted() {
-		//添加友盟埋点
-		const script = document.createElement("script");
-		script.src =
-			"https://s95.cnzz.com/z_stat.php?id=1260767143&web_id=1260767143";
-		script.language = "JavaScript";
-		document.body.appendChild(script);
+  },
+  mounted() {
+    //添加友盟埋点
+    const script = document.createElement("script");
+    script.src =
+      "https://s95.cnzz.com/z_stat.php?id=1260767143&web_id=1260767143";
+    script.language = "JavaScript";
+    document.body.appendChild(script);
 
     this.$router.beforeEach((to, from, next) => {
       this.getCreditResult(); //拉取征信报告
 
-			this.path = to.name;
-			if (to.meta.title) {
-				document.title = to.meta.title;
+      this.path = to.name;
+      if (to.meta.title) {
+        document.title = to.meta.title;
       }
-			if (to.matched.some(record => record.meta.requireAuth)) {
-				var userName = this.utils.getCookie("userName");
-				var realName = this.utils.getCookie("realName");
+      if (to.matched.some(record => record.meta.requireAuth)) {
+        var userName = this.utils.getCookie("userName");
+        var realName = this.utils.getCookie("realName");
         var mobile = this.utils.getCookie("mobile");
-      console.log('userName====', userName)
+        console.log("userName====", userName);
 
-				// 判断该路由是否需要登录权限
-				if (!userName || userName == "null") {
-					next({
-						// 将跳转的路由path作为参数，登录成功后跳转到该路由
-						path: "/login",
-						query: { redirect: to.fullPath }
-					});
-				} else {
-          console.info('next');
-					next();
-				}
-			} else {
-				next();
-			}
-		});
-
-	},
-	data() {
-		return {
-			path: this.$route.name,
-			platform: this.utils.getPlatform(),
+        // 判断该路由是否需要登录权限
+        if (!userName || userName == "null") {
+          next({
+            // 将跳转的路由path作为参数，登录成功后跳转到该路由
+            path: "/login",
+            query: { redirect: to.fullPath }
+          });
+        } else {
+          console.info("next");
+          next();
+        }
+      } else {
+        next();
+      }
+    });
+  },
+  data() {
+    return {
+      path: this.$route.name,
+      platform: this.utils.getPlatform(),
       authShowDialog: false,
       getReportSuccess: false,
-      couponAmount: '',
-      creditType: '',
-      creditRouterName: ''
-		};
-	},
-	methods: {
-    seeCoupon(){
+      couponAmount: "",
+      creditType: "",
+      creditRouterName: ""
+    };
+  },
+  methods: {
+    seeCoupon() {
       this.authShowDialog = false;
-      this.$router.push({'name': 'MyCoupon'});
+      this.$router.push({ name: "MyCoupon" });
     },
-		seeCredit(){
-      console.info('this.routerNameMapping', this.routerNameMapping());
+    seeCredit() {
+      console.info("this.routerNameMapping", this.routerNameMapping());
       this.authShowDialog = false;
-      this.$router.push({'name': this.routerNameMapping()});
+      this.$router.push({ name: this.routerNameMapping() });
     },
-    onConfirmReport(){
-      console.info('this.routerNameMapping', this.routerNameMapping());
+    onConfirmReport() {
+      console.info("this.routerNameMapping", this.routerNameMapping());
       this.getReportSuccess = false;
-      this.$router.push({'name': this.routerNameMapping()});
+      this.$router.push({ name: this.routerNameMapping() });
     },
-    getCreditResult(){
+    getCreditResult() {
       let userName = this.utils.getCookie("userName");
-      let whiteList = [ // 拉取报告白名单页面列表
-                        'Inquiry', 
-                        'IdentityAuth', 
-                        'PandoraAuth', 
-                        'Personal', 
-                        'Home', 
-                        'BreakPromise', 
-                        'operator',
-                        'jingdong',
-                        'Haluo',
-                        'Eleme',
-                        'ActivityIntroduction',
-                        'CreditFailedLoad',
-                        'CreditLoading'
-                      ]
-      if(!userName || whiteList.indexOf(this.path) < 0) return false;
+      let whiteList = [
+        // 拉取报告白名单页面列表
+        "Inquiry",
+        "IdentityAuth",
+        "PandoraAuth",
+        "Personal",
+        "Home",
+        "BreakPromise",
+        "operator",
+        "jingdong",
+        "Haluo",
+        "Eleme",
+        "ActivityIntroduction",
+        "CreditFailedLoad",
+        "CreditLoading"
+      ];
+      if (!userName || whiteList.indexOf(this.path) < 0) return false;
       let getCreditResultData = new URLSearchParams();
-				getCreditResultData.append("userName", this.utils.getCookie("userName"));
-      this.common.getCreditResult(getCreditResultData)
-      .then(res => {
-
-        if(res.data.resultCode != '1') return false;
+      getCreditResultData.append("userName", this.utils.getCookie("userName"));
+      this.common.getCreditResult(getCreditResultData).then(res => {
+        if (res.data.resultCode != "1") return false;
 
         this.creditType = res.data.creditType;
         let _params = {
-          "userName": res.data.userName,
-          "creditType": this.creditType
+          userName: res.data.userName,
+          creditType: this.creditType
         };
 
         this.checkCreditResult(_params);
       });
     },
-    checkCreditResult(_params){
-      this.common.CheckCreditResult(_params)
-      .then(res => {
+    checkCreditResult(_params) {
+      this.common.CheckCreditResult(_params).then(res => {
         let data = res.data.data;
-        if(data.firstRecevie){
+        if (data.firstRecevie) {
           this.couponAmount = data.amount;
           this.authShowDialog = true;
-        }else{
+        } else {
           this.getReportSuccess = true;
         }
       });
     },
-    routerNameMapping(){
-      let creditRouterName = '';
+    routerNameMapping() {
+      let creditRouterName = "";
       switch (this.creditType) {
-				case "operator": // * 运营商
-					creditRouterName = 'operator';
-					break;
-				case "jd": // * 京东
-					creditRouterName = 'jingdong';
-					break;
-				case "helloBike": // * 哈啰单车
-					creditRouterName = 'Haluo';
-					break;
-				case "eleme": // * 饿了么
-					creditRouterName = 'Eleme';
-					break;
-			}
-			return creditRouterName;
+        case "operator": // * 运营商
+          creditRouterName = "operator";
+          break;
+        case "jd": // * 京东
+          creditRouterName = "jingdong";
+          break;
+        case "helloBike": // * 哈啰单车
+          creditRouterName = "Haluo";
+          break;
+        case "eleme": // * 饿了么
+          creditRouterName = "Eleme";
+          break;
+      }
+      return creditRouterName;
     }
-	}
+  }
 };
 </script>
 
@@ -320,12 +326,12 @@ body {
   background-size: cover;
 }
 .icon-inquiry {
-	display: block;
-	width: 0.8rem;
-	height: 0.85rem;
-	margin-top: 0.15rem;
-	background: url("./assets/images/icon-inquiry.png") center center no-repeat;
-	background-size: cover;
+  display: block;
+  width: 0.8rem;
+  height: 0.85rem;
+  margin-top: 0.15rem;
+  background: url("./assets/images/icon-inquiry.png") center center no-repeat;
+  background-size: cover;
 }
 .icon-me {
   display: block;
@@ -336,33 +342,32 @@ body {
   background-size: cover;
 }
 .weui-bar__item_on {
-	.icon-home {
-		background: url("./assets/images/icon_home_act.png") center center
-			no-repeat;
-		background-size: cover;
-	}
-	.icon-vip-area {
-		background: url("./assets/images/icon_vip_area_act.png") center center
-			no-repeat;
-		background-size: cover;
-	}
-	.icon-broke-promise-area {
-		background: url("./assets/images/icon_broke_promise_area_act.png")
-			center center no-repeat;
-		background-size: cover;
-	}
-	.icon-me {
-		background: url("./assets/images/icon_me_act.png") center center
-			no-repeat;
-		background-size: cover;
-	}
-	.weui-tabbar__label {
-		color: #ff6700 !important;
-	}
-	.icon-inquiry {
-		background: url("./assets/images/icon-inquiry-select.png") center center no-repeat;
-		background-size: cover;
-	}
+  .icon-home {
+    background: url("./assets/images/icon_home_act.png") center center no-repeat;
+    background-size: cover;
+  }
+  .icon-vip-area {
+    background: url("./assets/images/icon_vip_area_act.png") center center
+      no-repeat;
+    background-size: cover;
+  }
+  .icon-broke-promise-area {
+    background: url("./assets/images/icon_broke_promise_area_act.png") center
+      center no-repeat;
+    background-size: cover;
+  }
+  .icon-me {
+    background: url("./assets/images/icon_me_act.png") center center no-repeat;
+    background-size: cover;
+  }
+  .weui-tabbar__label {
+    color: #ff6700 !important;
+  }
+  .icon-inquiry {
+    background: url("./assets/images/icon-inquiry-select.png") center center
+      no-repeat;
+    background-size: cover;
+  }
 }
 .weui-tabbar__item:last-child {
   .weui-tabbar__icon {
@@ -374,88 +379,87 @@ body {
 }
 
 .app-auth-dialog {
-	.weui-dialog{
-		background: transparent;
-		border-radius: 8px;
+  .weui-dialog {
+    background: transparent;
+    border-radius: 8px;
     padding-bottom: 8px;
     max-width: auto !important;
     top: 10% !important;
-	}
-	.dialog-title {
-		line-height: 30px;
-		color: #666;
-	}
-	.img-box {
+  }
+  .dialog-title {
+    line-height: 30px;
+    color: #666;
+  }
+  .img-box {
     position: relative;
     width: 100%;
-		img{
-			width: 100%;
+    img {
+      width: 100%;
     }
-    p{
+    p {
       width: 100%;
       position: absolute;
-			top: rem(100px);
-      color: #FF7E53;
+      top: rem(100px);
+      color: #ff7e53;
       font-size: rem(18px);
       text-align: center;
       font-weight: 500;
-      span{
-        color: #E63F06;
+      span {
+        color: #e63f06;
         font-weight: 600;
         font-size: rem(24px);
       }
     }
-		.btns{
-			width: 100%;
-			position: absolute;
-			bottom: rem(45px);
-			.auth-btn{
-				width: 70%;
-				height: rem(40px);
-				border: 0;
-				border-radius: 25px;
-				background: linear-gradient(#FFF2A7, #FFD96F); /* 标准的语法 */
-				color: #E63F06;
-				font-size: rem(15px);
-				display: block;
-				margin: 0 auto;
-				margin-bottom: rem(20px);
-			}
-		}
-	}
-	.vux-close {
-		width: rem(37px);
-		height: rem(37px);
-		background: url('./assets/images/pop-close-coupon.png') no-repeat;
-		display: block;
-		margin: rem(20px) auto;
-		background-size: rem(37px) rem(37px);
-	}
+    .btns {
+      width: 100%;
+      position: absolute;
+      bottom: rem(45px);
+      .auth-btn {
+        width: 70%;
+        height: rem(40px);
+        border: 0;
+        border-radius: 25px;
+        background: linear-gradient(#fff2a7, #ffd96f); /* 标准的语法 */
+        color: #e63f06;
+        font-size: rem(15px);
+        display: block;
+        margin: 0 auto;
+        margin-bottom: rem(20px);
+      }
+    }
+  }
+  .vux-close {
+    width: rem(37px);
+    height: rem(37px);
+    background: url("./assets/images/pop-close-coupon.png") no-repeat;
+    display: block;
+    margin: rem(20px) auto;
+    background-size: rem(37px) rem(37px);
+  }
 }
 
-.reportConfirm{
-  .weui-dialog__hd{
+.reportConfirm {
+  .weui-dialog__hd {
     padding: 0;
     height: 50px;
     line-height: 50px;
-    background: #FF7640;
+    background: #ff7640;
     font-size: 17px;
-    color: #FFFFFF;
+    color: #ffffff;
   }
-  .weui-dialog__bd{
+  .weui-dialog__bd {
     padding: 36px 0;
     font-size: 13px;
     color: #333333;
     letter-spacing: -0.08px;
     min-height: auto;
-
   }
-  .weui-dialog__btn_primary{
+  .weui-dialog__btn_primary {
     font-size: 15px;
-    color: #FF7640;
+    color: #ff7640;
     letter-spacing: -0.36px;
   }
-  .weui-dialog__btn_default{
+  .weui-dialog__btn_default {
     font-size: 15px;
     color: #999999;
     letter-spacing: -0.36px;
