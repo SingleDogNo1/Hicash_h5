7dc518be66662bb2f56584fc8af9c6d741061abb
wangzhe <wangzhe02@dpandora.cn>
2019-03-27 10:51:28 +0800

    .

--- src/pages/credit/Report/Operator.vue
+++ src/pages/credit/Report/Operator.vue
@@ -205,26 +205,6 @@ export default {
       wxShareIco: "./images/icon_share.png",
       showToast: true,
       thumbnailImg: "",
-      config: {
-        url: this.config.NEW_MWEB_PATH + "/activityIntroduction", // 网址，默认使用 window.location.href
-        source: "", // 来源（QQ空间会用到）, 默认读取head标签：<meta name="site" content="http://overtrue" />
-        title: this.title, // 标题，默认读取 document.title 或者 <meta name="title" content="share.js" />
-        description: "征信报告分享", // 描述, 默认读取head标签：<meta name="description" content="PHP弱类型的实现原理分析" />
-        image: this.wxShareIco, // 图片, 默认取网页中第一个img标签
-        //sites               : ['qzone', 'qq', 'weibo','wechat', 'douban'], // 启用的站点
-        disabled: [
-          "tencent",
-          "douban",
-          "linkedin",
-          "diandian",
-          "facebook",
-          "twitter",
-          "google"
-        ], // 禁用的站点
-        wechatQrcodeTitle: "微信扫一扫：分享", // 微信二维码提示文字
-        wechatQrcodeHelper:
-          "<p>微信里点“发现”，扫一下</p><p>二维码便可将本文分享至朋友圈。</p>"
-      },
       isShowWeixinPop: false,
       isWeiXinShare: false,
       isShowWeixinShareWrap: true
