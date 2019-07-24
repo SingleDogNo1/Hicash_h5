6e313b5576993997bd91c490e9c5e4fea8a08e99
wangzhe <wangzhe02@dpandora.cn>
2019-07-24 17:56:25 +0800

    sentry查看错误不混淆

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
-// const gitCommitInfo = require('child_process').execSync('git log -p -1 --pretty=format:"%s"').toString().split("diff --git")[0].trim();
-const gitCommitInfo = require('child_process').execSync('git log -p -1 --pretty=format:"%s"').toString().split("diff --git")[0].trim();
+const gitCommitInfo = require('child_process').execSync('/usr/local/git/bin/git log -p -1 --pretty=format:"%s"').toString().split("diff --git")[0].trim();
+//const gitCommitInfo = require('child_process').execSync('git log -p -1 --pretty=format:"%s"').toString().split("diff --git")[0].trim();
 process.env.RELEASE_VERSION = gitCommitInfo;
 console.log(`${gitCommitInfo}`)
 module.exports = {
