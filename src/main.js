6b8984323c2ae7973e568da495077901f38cedb3
wangzhe <wangzhe02@dpandora.cn>
2019-07-24 17:25:16 +0800

    sentry查看错误不混淆

--- src/main.js
+++ src/main.js
@@ -13,8 +13,8 @@ import {
 	AlertPlugin
 } from "vux"
 import "./assets/css/common.scss"
-import Raven from 'raven-js';
-import RavenVue from 'raven-js/plugins/vue';
+// import Raven from "raven-js"
+// import RavenVue from "raven-js/plugins/vue"
 
 //import vConsole from "vconsole"
 import * as Sentry from "@sentry/browser"
@@ -23,13 +23,15 @@ import * as Integrations from "@sentry/integrations"
 process.model === "dev" &&
 	Sentry.init({
 		dsn: "http://3da971d3d77645db926892adfd389c93@114.215.47.186:9000/3",
-		integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+		integrations: [new Integrations.Vue({ Vue, attachProps: true })],
+		release: process.sentryRelease
 	})
-
-Raven
-	.config('http://3da971d3d77645db926892adfd389c93@114.215.47.186:9000/3', {release: process.sentryRelease})
-	.addPlugin(RavenVue, Vue)
-	.install();
+console.log("process.env.RELEASE_VERSION==", process.env.RELEASE_VERSION)
+//Raven.config("http://3da971d3d77645db926892adfd389c93@114.215.47.186:9000/3", {
+// 	release: process.sentryRelease
+// })
+// 	.addPlugin(RavenVue, Vue)
+// 	.install()
 import common from "./api/common.js"
 import jsCommon from "./assets/js/common.js"
 import utils from "./assets/js/utils.js"
