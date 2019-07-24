61d441dc8ab9f706f0568db4b7beb84d1750be0f
wangzhe <wangzhe02@dpandora.cn>
2019-07-24 14:46:42 +0800

    sentry修改

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,6 +10,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const SentryCliPlugin = require('@sentry/webpack-plugin')
 
 //第一个参数（区分old还是new）
 const argv1 = process.argv[2];
@@ -126,9 +127,15 @@ const webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    new SentryCliPlugin({
+      release: process.env.RELEASE_VERSION,
+      include: 'dist/static/js'
+    })
   ]
 })
+
+console.log(":process.env.RELEASE_VERSION===", process.env.RELEASE_VERSION)
 
 if (config.build.productionGzip) {
   const CompressionWebpackPlugin = require('compression-webpack-plugin')
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,11 +1,15 @@
 'use strict'
 
-require('child_process').exec('git fetch --tags', () => {
-  const gitTag = require('child_process').execSync("echo -e $(git describe --tags `git rev-list --tags --max-count=1`)");
-  console.log("gitTag===", gitTag)
-});
-let gitSha = require('child_process').execSync('git rev-parse HEAD').toString().trim()
-console.log("gitSha===", gitSha)
+// require('child_process').exec('git fetch --tags', () => {
+//   const gitTag = require('child_process').execSync("echo -e $(git describe --tags `git rev-list --tags --max-count=1`)").toString().trim();
+//   console.log("gitTag===", gitTag)
+// });
+// var a = "git describe --tags $(git rev-list --tags --max-count=1)";
+// var b = "git describe --tags `git rev-list --tags --max-count=1`";
+// const gitTag = require('child_process').execSync(a).toString().trim();
+// console.log("gitTag===", gitTag)
+let gitCommitInfo = require('child_process').execSync('git log -p -1 --pretty=format:"%s"').toString().split("diff --git")[0].trim()
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: "production",
+  RELEASE_VERSION: `"${gitCommitInfo}"`
 }
