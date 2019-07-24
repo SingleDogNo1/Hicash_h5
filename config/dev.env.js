66251bd7831e3f6d1bdda1c95a74c6117a385ba1
wangzhe <wangzhe02@dpandora.cn>
2019-07-24 16:11:28 +0800

    sentry git修改

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,7 +2,8 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
-let gitCommitInfo = require('child_process').execSync('git log -p -1 --pretty=format:"%s"').toString().split("diff --git")[0].trim()
+let gitCommitInfo = require('child_process').execSync('/usr/local/git/bin/git log -p -1 --pretty=format:"%s"').toString().split("diff --git")[0].trim()
+// let gitCommitInfo = require('child_process').execSync('git log -p -1 --pretty=format:"%s"').toString().split("diff --git")[0].trim()
 console.log("gitCommitInfo===", gitCommitInfo)
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
