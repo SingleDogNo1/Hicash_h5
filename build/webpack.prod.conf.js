8138cb52765bda5a8534f14bdb6609c6c0399563
wangzhe <wangzhe02@dpandora.cn>
2019-07-25 10:15:36 +0800

    测试修改sentry

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -14,7 +14,6 @@ const SentryCliPlugin = require('@sentry/webpack-plugin')
 
 //第一个参数（区分old还是new）
 const argv1 = process.argv[2];
-
 // 因为本地和jenkins上面执行git命令的路径不同，做下判断
 const gitCommitInfo = argv1 && process.env.NODE_ENV === 'production' ?  require('child_process').execSync('/usr/local/git/bin/git log -p -1 --pretty=format:"%s"').toString().split("diff --git")[0].trim()
   : require('child_process').execSync('git log -p -1 --pretty=format:"%s"').toString().split("diff --git")[0].trim()
